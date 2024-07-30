78e15bf56343703a34326a1232d0298c55cdb58b
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2024-07-30 10:18:21 +0600

    fix typo and restructure project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Go Load Balancing
+# Go Load Balancer
+
+Load balancer for HTTP servers uses various strategies to balance the http traffic within a network of servers.
--- configs/config_test.go
+++ configs/config_test.go
@@ -1,4 +1,4 @@
-package main
+package configs
 
 import (
 	"fmt"
@@ -6,7 +6,7 @@ import (
 )
 
 func TestConfigParsing(t *testing.T) {
-	cfg, err := parseConfigFile("./tests/sample-config.json")
+	cfg, err := ParseLoadBalancerConfig("../tests/sample-config.json")
 	if err != nil {
 		t.Fatal(err)
 	}
